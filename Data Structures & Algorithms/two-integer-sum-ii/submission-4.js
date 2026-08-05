b0c802d020a499585b38e9f7e00e4d70871daaf6
Dickson Kinyua <167271003+dickson-kinyua@users.noreply.github.com>
2026-08-05 12:33:12 +0300

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        const n=numbers.length;
+        for(let i=1;i<n;i++){
+            for(let j=i+1;j<=n;j++){
+                if(numbers[i-1]+numbers[j-1] === target){
+                    return [i,j]
+                }
+
+            }
+           
+        }
+    }
+}
