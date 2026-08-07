b637ae79988fc35d80b01fbbf5c5a3c84c453631
Dickson Kinyua <167271003+dickson-kinyua@users.noreply.github.com>
2026-08-07 13:10:44 +0300

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        nums.sort((a,b)=>a-b)
+        const n=nums.length;
+        const result=[];
+        for(let i=0;i<n;i++){
+            if(nums[i]>0) break;
+            if(i>0 && nums[i]===nums[i-1]) continue;
+            let l=i+1;
+            let r=n-1;
+            while(l<r){
+                const sum=nums[i]+nums[l]+nums[r];
+                if(sum<0){
+                    l++;
+                }else if(sum>0){
+                    r--;
+                }else{
+                    result.push([nums[i],nums[l],nums[r]])
+                    l++;
+                    r--;
+                    while(l<r && nums[l]===nums[l-1]){
+                        l++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
